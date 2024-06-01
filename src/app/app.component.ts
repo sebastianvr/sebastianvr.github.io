3de04c0245d361605e220414b23de2c4a5711c61
Sebastian Vidal <sebastianvr63@gmail.com>
2024-06-01 15:17:40 -0400

    feat: Añadiendo todas las secciones creadas al componente principal app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutMyselfComponent } from './pages/about-myself/about-myself.component';
+
+const components = [
+  RouterOutlet,
+  SidenavComponent,
+  SkillsComponent,
+  FooterComponent,
+  ContactComponent,
+  AboutMyselfComponent
+];
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [...components],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sebastianvr.github.io';
+
+  isDarkMode: boolean = false;
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+  }
+
 }
