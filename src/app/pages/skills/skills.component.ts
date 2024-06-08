36d45d1569c24ab60bfb9cb3f24951ec64d33f3b
Sebastian Vidal <sebastianvr63@gmail.com>
2024-06-07 21:26:59 -0400

    feat: Añadiendo links de redicreccionamiento para herramientas utilizadas.

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,24 +1,29 @@
-import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Skill {
+  name: string,
+  path: string,
+  url: string,
+}
 
 @Component({
   selector: 'page-my-skills',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-  skills = [
-    { name: 'Node.js', path: 'nodejs' },
-    { name: 'Express', path: 'expressjs' },
-    { name: 'Angular', path: 'angular' },
-    { name: 'Sequelize', path: 'sequelize' },
-    { name: 'MySql', path: 'mysql' },
-    { name: 'Git', path: 'git' },
-    { name: 'GitHub', path: 'github' },
-    { name: 'Ng-bootstrap', path: 'ng-bootstrap' },
-    { name: 'Bootstrap', path: 'bootstrap' },
-    { name: 'Tailwind', path: 'tailwind' },
+  skills: Skill[] = [
+    { name: 'Git', path: 'git', url: 'https://www.git-scm.com/' },
+    { name: 'GitHub', path: 'github', url: 'https://github.com/' },
+    { name: 'Node.js', path: 'nodejs', url: 'https://nodejs.org/en' },
+    { name: 'Express', path: 'expressjs', url: 'https://expressjs.com/es/' },
+    { name: 'Sequelize', path: 'sequelize', url: 'https://sequelize.org/' },
+    { name: 'MySql', path: 'mysql', url: 'https://www.mysql.com/' },
+    { name: 'Angular', path: 'angular', url: 'https://angular.dev/' },
+    { name: 'Bootstrap', path: 'bootstrap', url: 'https://getbootstrap.com/' },
+    { name: 'Tailwind', path: 'tailwind', url: 'https://tailwindcss.com/' },
+    { name: 'Ng-bootstrap', path: 'ng-bootstrap', url: 'https://ng-bootstrap.github.io/#/home' },
   ];
 }
