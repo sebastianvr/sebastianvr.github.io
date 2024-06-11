6dd271f0ac1f50b7791b91397a07d204038b86f5
Sebastian Vidal <sebastianvr63@gmail.com>
2024-06-10 21:44:18 -0400

    feat: Nuevo componenete work-experience.

---
+++ src/app/pages/work-experience/work-experience.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-work-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-experience.component.html',
+  styleUrl: './work-experience.component.css'
+})
+export class WorkExperienceComponent {
+
+}
