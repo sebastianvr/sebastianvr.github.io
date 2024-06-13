62e201fcae09a2eefa911a3140ec5ea7ef7f479a
Sebastian Vidal <sebastianvr63@gmail.com>
2024-06-12 22:04:21 -0400

    feat: Implementando gsap para cards de skills.

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { gsap } from 'gsap';
 
+import { CSSPlugin } from 'gsap/CSSPlugin';
+
+gsap.registerPlugin(CSSPlugin);
 interface Skill {
   name: string,
   path: string,
@@ -13,7 +17,7 @@ interface Skill {
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
-export class SkillsComponent {
+export class SkillsComponent implements AfterViewInit {
   skills: Skill[] = [
     { name: 'Git', path: 'git', url: 'https://www.git-scm.com/' },
     { name: 'GitHub', path: 'github', url: 'https://github.com/' },
@@ -25,5 +29,35 @@ export class SkillsComponent {
     { name: 'Bootstrap', path: 'bootstrap', url: 'https://getbootstrap.com/' },
     { name: 'Tailwind', path: 'tailwind', url: 'https://tailwindcss.com/' },
     { name: 'Ng-bootstrap', path: 'ng-bootstrap', url: 'https://ng-bootstrap.github.io/#/home' },
+    { name: 'NestJs', path: 'nest-js', url: 'https://nestjs.com/' },
+    { name: 'Mongo-DB', path: 'mongo-db', url: 'https://www.mongodb.com/es' },
   ];
+  @ViewChildren('skill') skillElements!: ElementRef[];
+
+  ngAfterViewInit(): void {
+    this.skillElements.forEach((element, index) => {
+      gsap.from(element.nativeElement, {
+        scale: 0.9,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        duration: 0.3,
+        ease: 'power2.inOut',
+      });
+      element.nativeElement.addEventListener('mouseenter', () => {
+        gsap.to(element.nativeElement, {
+          scale: 1,
+          backgroundColor: 'rgba(255, 255, 255, 1)',
+          duration: 0.3,
+          ease: 'power2.inOut',
+        });
+      });
+      element.nativeElement.addEventListener('mouseleave', () => {
+        gsap.to(element.nativeElement, {
+          scale: 0.9,
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
+          duration: 0.3,
+          ease: 'power2.inOut',
+        });
+      });
+    });
+  }
 }
