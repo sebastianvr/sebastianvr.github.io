b10922d7390a93b9e8c9acac44029a9dd18bc7d1
Sebastian Vidal <sebastianvr63@gmail.com>
2024-06-18 18:05:36 -0400

    feat: implementando gtag para google analitics.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { GtagModule } from 'angular-gtag';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-   providers: [provideHttpClient(withFetch())]
+   providers: [
+    provideHttpClient(withFetch()),
+    GtagModule.forRoot({ 
+      trackingId : 'G-ZEZRB0309D',
+      trackPageviews : true,
+    }),
+   ]
  })
   .catch((err) => console.error(err));
