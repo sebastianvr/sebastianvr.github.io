f7dc4fad68c27fa01a022b904fa6462617c625ba
Sebastian Vidal <sebastianvr63@gmail.com>
2024-06-22 14:05:28 -0400

    feat: Modificación de tailwind.config para manejar flowbite.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'xxs': '0.65rem', // Define el tamaño de texto 'xxs' (10px)
+      },
+      padding: {
+        '0.25': '0.0625rem', // Define el padding '0.25' (1px)
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
